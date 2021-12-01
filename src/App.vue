419e6db9207c0e66053fc1bba3d146ba3e7ea01a
Stanislav Elshaidt <stanislav.elshaidt@saritasa.com>
2021-12-01 22:57:55 +0600

    Experiments to fetch data-2

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,8 @@
 </template>
 <script>
 import { defineComponent, provide } from 'vue'
-import { DefaultApolloClient, ApolloClients} from '@vue/apollo-composable'
-import {ApolloClient, InMemoryCache, gql} from '@apollo/client/core';
-
-const apolloClient = new ApolloClient({
-  uri: 'https://tiktok.ufobe.com/api/graphql',
-  cache: new InMemoryCache()
-})
 
 export default defineComponent({
   name: 'App',
-  setup() {
-    provide(ApolloClients, apolloClient)
-  },
 })
 </script>
